a0c4b08e4d5c87f1f3253adfb59be882960c6d5d
Simon Hinfray <shinfray@student.s19.be>
2022-10-19 12:45:05 +0200

    update

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:50:53 by shinfray          #+#    #+#             */
-/*   Updated: 2022/10/19 10:52:44 by shinfray         ###   ########.fr       */
+/*   Updated: 2022/10/19 12:31:50 by simonhinf        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	void	*saved_next;
 
-
-
-
-
-
+	if (lst == NULL || (del) == NULL)
+		return ;
+	if (*lst == NULL)
+		return ;
+	else
+	{
+		while (*lst != NULL)
+		{
+			saved_next = (*lst)->next;
+			ft_lstdelone(*lst, (del));
+			*lst = saved_next;
+		}
+	}
 }
