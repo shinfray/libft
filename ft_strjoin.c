03793dc30f2acafd43babe849d57673f74fb0bd3
shinfray <simon@hinfray.eu>
2022-12-18 01:19:34 +0100

    update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:23:15 by shinfray          #+#    #+#             */
-/*   Updated: 2022/10/17 03:55:17 by shinfray         ###   ########.fr       */
+/*   Updated: 2022/12/18 00:53:33 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*ptr;
 	size_t	s1_len;
 	size_t	s2_len;
+	char	*ptr;
 
-	ptr = NULL;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	s1_len = ft_strlen(s1);
@@ -27,6 +26,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (ptr == NULL)
 		return (NULL);
 	ft_memcpy(ptr, s1, s1_len);
-	ft_memcpy(ptr + s1_len, s2, s2_len + 1);
+	ft_memcpy(ptr + s1_len, s2, s2_len);
 	return (ptr);
 }
